f8cc9e6082952a19363571c81db5a819022395af
BaptisteViorney <128126292+Kaelhys@users.noreply.github.com>
2023-03-30 14:06:32 +0200

    Update Player.java

--- A1/Projet/Player.java
+++ A1/Projet/Player.java
@@ -1,15 +1,19 @@
-public class Player extends Wizard{
+public class Player extends Wizard {
     public int currentHealth;
-    //Variables / Attributs d'un joueur
+    // Variables / Attributs d'un joueur
     public House house;
     public int numATKupgrades, numDEFupgrades, numMAGupgrades;
 
-    //Constructeur
-    public Player(String name) {
-        super(name, 100, 0, 100, 100);
+    // Constructeur
+    public Player() {
+        super("Joueur", 100, 100, 0, 100, 100);
+        this.currentHealth = 100;
+        this.numATKupgrades = 0;
+        this.numDEFupgrades = 0;
+        this.numMAGupgrades = 0;
     }
 
-    //getters/setters
+    // getters/setters
     public void setHouse(House house) {
         this.house = house;
     }
@@ -21,12 +25,20 @@ public class Player extends Wizard{
     @Override
     public int attack() {
         // TODO Auto-generated method stub
-        return (int) (Math.random()*(this.numATKupgrades + 3 + this.level));
+        return (int) (Math.random() * (this.numATKupgrades + 3 + this.level));
     }
 
     @Override
     public int defend() {
         // TODO Auto-generated method stub
-        return (int) (Math.random()*(this.numDEFupgrades + 3 + this.level));
+        return (int) (Math.random() * (this.numDEFupgrades + 3 + this.level));
+    }
+
+    public int getMaxHealth() {
+        return this.maxHealth;
+    }
+
+    public int getMaxMagic() {
+        return this.maxMagic;
     }
 }
