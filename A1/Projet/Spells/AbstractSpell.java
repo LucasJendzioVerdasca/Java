05b072ec6603ea69efcf81dd46ea51e2f5d81f2d
Lucas_Jendzio-Verdasca <117655091+LucasJendzioVerdasca@users.noreply.github.com>
2023-03-31 14:12:51 +0200

    Add files via upload

---
+++ A1/Projet/Spells/AbstractSpell.java
@@ -0,0 +1,25 @@
+package Spells;
+public abstract class AbstractSpell {
+    protected String name;
+    private int magicCost;
+    private int power;
+
+    // Constructeurs
+    public AbstractSpell() {}
+
+    public AbstractSpell(String name, int magicCost, int power) {
+        this.name = name;
+        this.magicCost = magicCost;
+        this.power = power;
+    }
+
+    // Constantes
+    public static final int LOW_MAGIC_COST = 10;
+    public static final int MEDIUM_MAGIC_COST = 20;
+    public static final int HIGH_MAGIC_COST = 30;
+
+    
+    public String toString() {
+        return "Name: " + name + ", Magic Cost: " + magicCost + ", Power: " + power;
+    }
+}
