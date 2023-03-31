28da9c11341904b3be90d5a8de595500eeb4cf81
Lucas_Jendzio-Verdasca <117655091+LucasJendzioVerdasca@users.noreply.github.com>
2023-03-31 14:07:56 +0200

    Create Main.java

---
+++ A1/Projet/Main.java
@@ -0,0 +1,9 @@
+import controller.GameLogic;
+
+public class Main {
+    public static void main(String[] args) {
+        // Démarrage du jeu
+        var game = new GameLogic();
+        game.startGame();
+    }
+}
