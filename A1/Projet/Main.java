1cf560572c868104c56942a1fe64879b0262b3bc
Lucas_Jendzio-Verdasca <117655091+LucasJendzioVerdasca@users.noreply.github.com>
2023-04-21 11:47:49 +0200

    Delete A1/Projet directory

--- A1/Projet/Main.java
+++
@@ -1,9 +0,0 @@
-import controller.GameLogic;
-
-public class Main {
-    public static void main(String[] args) {
-        // Démarrage du jeu
-        var game = new GameLogic();
-        game.startGame();
-    }
-}
